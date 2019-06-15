4576ef0e8ba71a52f638936b0791a27997c37bbd
kingcreek <kingcreek@kingcreek.local>
2019-06-15 13:04:53 +0200

    p

--- get_info.js
+++ get_info.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const fs   = require('fs');
 const ytdl = require('./lib/index');
+const util = require('util')
+
 const id  = 'qR9fuHodXG4';
 
 const filepath = path.resolve(__dirname, 'info.json');
@@ -10,7 +12,7 @@ ytdl.getInfo(id, (err, info) => {
   console.log('title:', info.title);
   console.log('rating:', info.avg_rating);
   console.log('uploaded by:', info.author.name);
-  console.log('uploaded by:', info.streamingData.formats.url);
+  console.log(util.inspect(info, {showHidden: false, depth: null}))
   const json = JSON.stringify(info, null, 2)
     .replace(/(ip(?:=|%3D|\/))((?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)|[0-9a-f]{1,4}(?:(?::|%3A)[0-9a-f]{1,4}){7})/ig, '$10.0.0.0');
   fs.writeFile(filepath, json, err => {
